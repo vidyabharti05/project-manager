4f18e27cd9f2aea6bb2562a4ade4ac1109ae834f
vidya bharti <vidyabharti2605@gmail.com>
2026-09-08 20:10:46 +0530

    Refactor API_URL to use environment variable
    Update API_URL to use environment variable or fallback.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,8 +12,8 @@ import {
 
 import Link from "next/link";
 
-const API_URL = "http://localhost:8080";
-
+const API_URL =  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:8080";
 type TaskForm = {
   title: string;
   description: string;
